78d7758f7abf127eb4de5c918a06e3bbe18d0763
illilyelansary <illilyelansary@gmail.com>
2025-07-18 17:26:12 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthContext';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
