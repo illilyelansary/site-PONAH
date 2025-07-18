a48fe1c1aa05ae0f210e658a505a2a110e1219a7
illilyelansary <illilyelansary@gmail.com>
2025-07-18 23:54:06 +0000

    Update publicationsData.js

--- src/data/publicationsData.js
+++ src/data/publicationsData.js
@@ -5,37 +5,43 @@ const publications = [
     title: 'Brochure de Présentation de la PONAH',
     description: 'Document officiel de présentation de la plateforme.',
     date: '2024',
-    image: '/images/brochure.jpg'
+    image: '/images/brochure.jpg',
+    slug: 'brochure-de-presentation-de-la-ponah'
   },
   {
     title: 'Rapport Annuel 2024',
     description: 'Activités, réalisations et défis de l\'année.',
     date: 'Décembre 2024',
-    image: '/images/rapport2024.jpg'
+    image: '/images/rapport2024.jpg',
+    slug: 'rapport-annuel-2024'
   },
   {
     title: 'Document de Lobby Tour',
     description: 'Recommandations pour la mobilisation des ressources.',
     date: 'Juin 2024',
-    image: '/images/lobbytour.jpg'
+    image: '/images/lobbytour.jpg',
+    slug: 'document-de-lobby-tour'
   },
   {
     title: 'Étude du Baromètre de la Localisation',
     description: 'Évaluation de la localisation des ONG nationales.',
     date: 'Octobre 2024',
-    image: '/images/etude-barometre.jpg'
+    image: '/images/etude-barometre.jpg',
+    slug: 'etude-du-barometre-de-la-localisation'
   },
   {
     title: 'Statuts & Règlement Intérieur',
     description: 'Documents régissant la PONAH.',
     date: 'Janvier 2024',
-    image: '/images/statuts.jpg'
+    image: '/images/statuts.jpg',
+    slug: 'statuts-et-reglement-interieur'
   },
   {
     title: 'Charte des Membres',
     description: 'Droits et devoirs des membres de la plateforme.',
     date: 'Janvier 2024',
-    image: '/images/charte.jpg'
+    image: '/images/charte.jpg',
+    slug: 'charte-des-membres'
   }
 ];
 
