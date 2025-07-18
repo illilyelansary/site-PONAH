92052a210a382a7efdca2523f89109aab58b5529
illilyelansary <illilyelansary@gmail.com>
2025-07-18 23:30:26 +0000

    Update newsData.js

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -4,6 +4,7 @@ const news = [
   {
     id: 1,
     title: 'Validation de la Stratégie Nationale de Localisation',
+    slug: 'validation-strategie-nationale-localisation',
     excerpt: 'Les 9 axes stratégiques ont été validés lors de l\'atelier de restitution.',
     date: '30 octobre 2024',
     image: '/images/news1-new.jpg'
@@ -11,6 +12,7 @@ const news = [
   {
     id: 2,
     title: 'Formation de 360 acteurs sur la gestion des risques de sécurité',
+    slug: 'formation-360-acteurs-gestion-risques-securite',
     excerpt: '72 OSC ont bénéficié d\'une formation complète dans 3 régions.',
     date: '15 décembre 2024',
     image: '/images/news2.jpg'
@@ -18,6 +20,7 @@ const news = [
   {
     id: 3,
     title: 'Mission de plaidoyer fructueuse au Qatar',
+    slug: 'mission-plaidoyer-fructueuse-qatar',
     excerpt: 'Des promesses de financement et de nouveaux partenariats ont été obtenus.',
     date: '20 septembre 2024',
     image: '/images/news3.jpg'
@@ -25,6 +28,7 @@ const news = [
   {
     id: 4,
     title: 'Lancement du Baromètre de la Localisation',
+    slug: 'lancement-barometre-localisation',
     excerpt: 'Les bases méthodologiques pour évaluer les progrès ont été posées.',
     date: '30 mai 2024',
     image: '/images/news4.jpg'
@@ -32,6 +36,7 @@ const news = [
   {
     id: 5,
     title: 'Table Ronde sur l\'état d\'avancement des recommandations',
+    slug: 'table-ronde-etat-avancement-recommandations',
     excerpt: 'Point d\'étape sur les 13 recommandations du Forum National.',
     date: '3 mai 2024',
     image: '/images/news5.jpg'
@@ -39,6 +44,7 @@ const news = [
   {
     id: 6,
     title: 'Création officielle de la PONAH',
+    slug: 'creation-officielle-ponah',
     excerpt: 'La PONAH a été officiellement enregistrée en janvier 2024.',
     date: '18 janvier 2024',
     image: '/images/news6.jpg'
