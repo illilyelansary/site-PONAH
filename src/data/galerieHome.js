202b123c7302ae3d3b088506af0ba52b5f404421
illilyelansary <illilyelansary@gmail.com>
2025-07-18 15:35:27 +0000

    Update galerieHome.js

--- src/data/galerieHome.js
+++ src/data/galerieHome.js
@@ -1,44 +1,82 @@
-import barometreRestitution from '../assets/actualites/barometre-restitution.jpg';
-import formation360 from '../assets/actualites/formation-360.jpg';
-import missionQatar from '../assets/actualites/mission-qatar.jpg';
-import rapportAnnuel from '../assets/publications/rapport-2024.jpg';
+// src/data/galerieHome.js
+
+import event1 from '../assets/event1.jpg';
+import event2 from '../assets/event2.jpg';
+import event3 from '../assets/event3.jpg';
+import news1 from '../assets/news1.jpg';
+import news2 from '../assets/news2.jpg';
+import news3 from '../assets/news3.jpg';
+import publication1 from '../assets/publication1.jpg';
+import publication2 from '../assets/publication2.jpg';
+import publication3 from '../assets/publication3.jpg';
 
 export const galerieHome = [
-  // Activités
+  // Actualités (tirées de News.jsx)
   {
-    type: 'Activité',
-    slug: 'barometre-restitution',
+    title: 'Validation de la Stratégie Nationale de Localisation',
+    date: '30 octobre 2024',
+    image: news1,
+    type: 'Actualité',
+    link: '/actualites'
+  },
+  {
+    title: 'Formation de 360 acteurs sur les risques de sécurité',
+    date: '15 décembre 2024',
+    image: news2,
+    type: 'Actualité',
+    link: '/actualites'
+  },
+  {
+    title: 'Mission de plaidoyer fructueuse au Qatar',
+    date: '20 septembre 2024',
+    image: news3,
+    type: 'Actualité',
+    link: '/actualites'
+  },
+
+  // Activités (tirées de Activities.jsx)
+  {
     title: 'Atelier du Baromètre de la Localisation',
-    date: '29-30 octobre 2024',
-    image: barometreRestitution,
-    link: '/actualites/barometre-restitution'
-  },
-  {
+    date: '29-30 mai 2024',
+    image: event1,
     type: 'Activité',
-    slug: 'formation-360',
-    title: 'Formation sur la gestion des risques',
+    link: '/activites'
+  },
+  {
+    title: 'Formation de 360 OSC sur la Sécurité',
     date: 'Nov-Déc 2024',
-    image: formation360,
-    link: '/actualites/formation-360'
+    image: event2,
+    type: 'Activité',
+    link: '/activites'
   },
   {
+    title: 'Lobby Tour aux États-Unis',
+    date: 'Juin 2024',
+    image: event3,
     type: 'Mission',
-    slug: 'mission-qatar',
-    title: 'Plaidoyer humanitaire au Qatar',
-    date: '16-20 sept. 2024',
-    image: missionQatar,
-    link: '/actualites/mission-qatar'
+    link: '/activites'
   },
 
-  // Publication
+  // Publications (tirées de Publications.jsx)
   {
+    title: 'Rapport Annuel 2024',
+    date: 'Décembre 2024',
+    image: publication1,
     type: 'Publication',
-    slug: 'rapport-annuel-2024',
-    title: 'Rapport Annuel de la PONAH 2024',
-    date: 'Mars 2025',
-    image: rapportAnnuel,
-    link: '/publications#rapport-annuel-2024'
+    link: '/publications'
+  },
+  {
+    title: 'Document de Lobby Tour',
+    date: 'Juin 2024',
+    image: publication2,
+    type: 'Publication',
+    link: '/publications'
+  },
+  {
+    title: 'Étude du Baromètre de la Localisation',
+    date: 'Octobre 2024',
+    image: publication3,
+    type: 'Étude',
+    link: '/publications'
   }
-
-  // Tu peux ajouter ici d’autres actualités ou documents
 ];
