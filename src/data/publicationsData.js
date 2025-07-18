71a8e001069850e6118feb1c982a75c393b0ad75
illilyelansary <illilyelansary@gmail.com>
2025-07-18 17:42:17 +0000

    Create publicationsData.js

---
+++ src/data/publicationsData.js
@@ -0,0 +1,42 @@
+// src/data/publicationsData.js
+
+const publications = [
+  {
+    title: 'Brochure de Présentation de la PONAH',
+    description: 'Document officiel de présentation de la plateforme.',
+    date: '2024',
+    image: '/images/brochure.jpg'
+  },
+  {
+    title: 'Rapport Annuel 2024',
+    description: 'Activités, réalisations et défis de l\'année.',
+    date: 'Décembre 2024',
+    image: '/images/rapport2024.jpg'
+  },
+  {
+    title: 'Document de Lobby Tour',
+    description: 'Recommandations pour la mobilisation des ressources.',
+    date: 'Juin 2024',
+    image: '/images/lobbytour.jpg'
+  },
+  {
+    title: 'Étude du Baromètre de la Localisation',
+    description: 'Évaluation de la localisation des ONG nationales.',
+    date: 'Octobre 2024',
+    image: '/images/etude-barometre.jpg'
+  },
+  {
+    title: 'Statuts & Règlement Intérieur',
+    description: 'Documents régissant la PONAH.',
+    date: 'Janvier 2024',
+    image: '/images/statuts.jpg'
+  },
+  {
+    title: 'Charte des Membres',
+    description: 'Droits et devoirs des membres de la plateforme.',
+    date: 'Janvier 2024',
+    image: '/images/charte.jpg'
+  }
+];
+
+export default publications;
