014a17fc853d531f55f41091de6b4c6c16f541ee
illilyelansary <illilyelansary@gmail.com>
2025-07-18 17:40:37 +0000

    Create newsData.js

---
+++ src/data/newsData.js
@@ -0,0 +1,48 @@
+// src/data/newsData.js
+
+const news = [
+  {
+    id: 1,
+    title: 'Validation de la Stratégie Nationale de Localisation',
+    excerpt: 'Les 9 axes stratégiques ont été validés lors de l\'atelier de restitution.',
+    date: '30 octobre 2024',
+    image: '/images/news1-new.jpg'
+  },
+  {
+    id: 2,
+    title: 'Formation de 360 acteurs sur la gestion des risques de sécurité',
+    excerpt: '72 OSC ont bénéficié d\'une formation complète dans 3 régions.',
+    date: '15 décembre 2024',
+    image: '/images/news2.jpg'
+  },
+  {
+    id: 3,
+    title: 'Mission de plaidoyer fructueuse au Qatar',
+    excerpt: 'Des promesses de financement et de nouveaux partenariats ont été obtenus.',
+    date: '20 septembre 2024',
+    image: '/images/news3.jpg'
+  },
+  {
+    id: 4,
+    title: 'Lancement du Baromètre de la Localisation',
+    excerpt: 'Les bases méthodologiques pour évaluer les progrès ont été posées.',
+    date: '30 mai 2024',
+    image: '/images/news4.jpg'
+  },
+  {
+    id: 5,
+    title: 'Table Ronde sur l\'état d\'avancement des recommandations',
+    excerpt: 'Point d\'étape sur les 13 recommandations du Forum National.',
+    date: '3 mai 2024',
+    image: '/images/news5.jpg'
+  },
+  {
+    id: 6,
+    title: 'Création officielle de la PONAH',
+    excerpt: 'La PONAH a été officiellement enregistrée en janvier 2024.',
+    date: '18 janvier 2024',
+    image: '/images/news6.jpg'
+  }
+];
+
+export default news;
