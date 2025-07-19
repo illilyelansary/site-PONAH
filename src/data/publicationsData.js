53ec2aba55e2941de71241c3e232d1fdfd759df2
illilyelansary <illilyelansary@gmail.com>
2025-07-19 00:47:33 +0000

    Update publicationsData.js

--- src/data/publicationsData.js
+++ src/data/publicationsData.js
@@ -2,46 +2,31 @@
 
 const publications = [
   {
+    id: 1,
     title: 'Brochure de Présentation de la PONAH',
-    description: 'Document officiel de présentation de la plateforme.',
-    date: '2024',
-    image: '/images/brochure.jpg',
-    slug: 'brochure-de-presentation-de-la-ponah'
+    description: 'Une vue d’ensemble de la Plateforme des ONG Humanitaires nationales.',
+    date: 'Janvier 2024',
+    image: '/images/publication1.jpg',
+    slug: 'brochure-presentation-ponah-2024',
+    link: '/publications/brochure-presentation-ponah-2024'
   },
   {
-    title: 'Rapport Annuel 2024',
-    description: 'Activités, réalisations et défis de l\'année.',
+    id: 2,
+    title: 'Stratégie de Localisation au Mali',
+    description: 'Document de référence pour la mise en œuvre de la localisation humanitaire.',
+    date: 'Mai 2024',
+    image: '/images/publication2.jpg',
+    slug: 'strategie-localisation-mali-2024',
+    link: '/publications/strategie-localisation-mali-2024'
+  },
+  {
+    id: 3,
+    title: 'Rapport d’Activités Annuel 2024',
+    description: 'Récapitulatif des missions, actions et partenariats de la PONAH.',
     date: 'Décembre 2024',
-    image: '/images/rapport2024.jpg',
-    slug: 'rapport-annuel-2024'
-  },
-  {
-    title: 'Document de Lobby Tour',
-    description: 'Recommandations pour la mobilisation des ressources.',
-    date: 'Juin 2024',
-    image: '/images/lobbytour.jpg',
-    slug: 'document-de-lobby-tour'
-  },
-  {
-    title: 'Étude du Baromètre de la Localisation',
-    description: 'Évaluation de la localisation des ONG nationales.',
-    date: 'Octobre 2024',
-    image: '/images/etude-barometre.jpg',
-    slug: 'etude-du-barometre-de-la-localisation'
-  },
-  {
-    title: 'Statuts & Règlement Intérieur',
-    description: 'Documents régissant la PONAH.',
-    date: 'Janvier 2024',
-    image: '/images/statuts.jpg',
-    slug: 'statuts-et-reglement-interieur'
-  },
-  {
-    title: 'Charte des Membres',
-    description: 'Droits et devoirs des membres de la plateforme.',
-    date: 'Janvier 2024',
-    image: '/images/charte.jpg',
-    slug: 'charte-des-membres'
+    image: '/images/publication3.jpg',
+    slug: 'rapport-annuel-ponah-2024',
+    link: '/publications/rapport-annuel-ponah-2024'
   }
 ];
 
